0cba113cb702dd79a04b824b5dbbf16fcd3d5566
GrG <gregory.besson@adfab.fr>
2014-03-16 10:35:49 -0700

    First release

---
+++ detectbandwidth.js
@@ -0,0 +1,62 @@
+"use strict";
+
+self.addEventListener('message', function (e) {
+    var data, url, start, fileSize;
+    
+    data = e.data;
+    url = data.testFile;
+    
+    start=new Date();
+    start=start.getTime();
+    getFileSize();
+    analyze();
+
+    function getFileSize() {
+        debugger;
+        try {
+            var xhr = new XMLHttpRequest();
+            xhr.open('HEAD', url, false);
+            //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        fileSize = xhr.getResponseHeader('Content-Length');
+                    }
+                }
+            };
+            xhr.send(null);
+        } catch (e) {
+
+        }
+    }
+    
+    function analyze() {
+        debugger;
+        try {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, false);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        result();
+                    }
+                }
+            };
+            xhr.send(null);
+        } catch (e) {
+            postMessage(null);
+        }
+    }
+     
+    function result(v) {
+        var end, rate, duration;
+        
+        end = new Date();
+        end = end.getTime();
+        duration = end-start;
+        rate = Math.round( (fileSize/(end-start)) *10 ) /10;
+        
+        postMessage("{'fileSize':" + fileSize + ",'rate':"+ rate + ",'duration':" + duration + "}");
+    }
+}, false);
